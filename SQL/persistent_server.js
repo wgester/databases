588dc47c86ab14ed9adba27e37efbd3682fa5661
Hack Reactor Students <willgester@gmail.com>
2014-01-31 15:06:41 -0800

    Created sql database and insertion method

--- SQL/persistent_server.js
+++ SQL/persistent_server.js
@@ -6,12 +6,23 @@ var mysql = require('mysql');
  * database: "chat" specifies that we're using the database called
  * "chat", which we created by running schema.sql.*/
 var dbConnection = mysql.createConnection({
-  user: "",
+  user: "root",
   password: "",
-  database: "chat"
+  database: "chatterBoxxx"
 });
 
 dbConnection.connect();
+
+var submitMessage = function(message, username, roomname){
+  var date = new Date();
+  dbConnection.query("insert into messages (message, userID, roomnameID) values (\'" + message + "\', \'" + username + "\', \'" +roomname + "\')", function(error, result){
+    if (error) console.log(error);
+    console.log('this message has been sent:', message);
+  });
+};
+
+submitMessage('hello this is a test', '1', 1);
+
 /* Now you can make queries to the Mysql database using the
  * dbConnection.query() method.
  * See https://github.com/felixge/node-mysql for more details about
--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,13 +1,90 @@
-CREATE DATABASE chat;
+CREATE DATABASE chatterBoxxx;
 
-USE chat;
+USE chatterBoxxx;
 
-CREATE TABLE messages (
+
  /* Describe your table here.*/
-);
 
 /* You can also create more tables, if you need them... */
 
 /*  Execute this file from the command line by typing:
  *    mysql < schema.sql
  *  to create the database and the tables.*/
+
+
+
+
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+--
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+    
+CREATE TABLE `messages` (
+  `id` TINYINT NULL AUTO_INCREMENT DEFAULT NULL,
+  `message` VARCHAR(255) NULL DEFAULT NULL,
+  `date` DATE NULL DEFAULT NULL,
+  `userID` TINYINT NULL DEFAULT NULL,
+  `roomnameID` TINYINT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'usernames'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `usernames`;
+    
+CREATE TABLE `usernames` (
+  `id` TINYINT NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` VARCHAR(25) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'roomnames'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `roomnames`;
+    
+CREATE TABLE `roomnames` (
+  `id` TINYINT NULL AUTO_INCREMENT DEFAULT NULL,
+  `roomname` VARCHAR(25) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (userID) REFERENCES `usernames` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (roomnameID) REFERENCES `roomnames` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `usernames` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `roomnames` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`message`,`date`,`time`,`userID`,`roomnameID`) VALUES
+-- ('','','','','','');
+-- INSERT INTO `usernames` (`id`,`username`) VALUES
+-- ('','');
+-- INSERT INTO `roomnames` (`id`,`roomname`) VALUES
+-- ('','');
